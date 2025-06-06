dae5e65bc9a0ee959d86111160b01520d9a97680
coopercodes3 <thecoopercodes@gmail.com>
2025-06-06 12:04:01 -0700

    first

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import '../app.css';
+	
+	let { children } = $props();
+</script>
+
+
+<div class="relative sticky left-1/2 transform -translate-x-1/2  flex items-center mt-[24px] py-2.5 w-[700px] bg-[#273F4F] rounded-[8px]">
+    <div class="flex justify-between  mx-4 w-full">
+        <div class="flex items-center">
+            <p class="text-white font-bold text-lg">Cooper Lappenbusch</p>
+            <div class="flex items-center space-x-6 ml-6 text-white">
+                <p>Blog</p>
+                <p>Projects</p>
+            </div>
+        </div>
+
+        <button class="bg-[#FE7743] hover:bg-black hover:text-[#FE7743] border border-transparent hover:border-[#FE7743]  transition-all duration-200 cursor-pointer px-4 text-white rounded-[8px] font-extrabold">
+            Reach Out
+        </button>
+    </div>
+</div>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+
+
+
+<div class=" h-[800px]  pt-[100px] flex flex-col items-center ">
+
+    <div class="w-[700px] h-[400px] rounded-[10px] relative overflow-hidden shadow-2xl" 
+            style="background-image: url('arcanaScreen.png'); background-size: cover; background-position: center;">
+        
+        <div class="absolute bottom-0 left-0 w-full h-32 bg-gradient-to-b to-black from-transparent to-[85%] rounded-bl-[10px]"></div>
+        
+        <!-- Corner text only -->
+        <div class="absolute bottom-5 left-8 text-white">
+            <p class="text-xl font-bold mb-0.5 drop-shadow-lg">Arcana</p>
+            <p class="text-sm opacity-90 drop-shadow-md">Time tracking in 3 clicks</p>
+        </div>
+    </div>
+
+
+    <div class="flex space-x-4 mt-8">
+        <div class="w-16 h-3 bg-[#FE7743]"></div>
+        <div class="w-16 h-3 bg-neutral-500"></div>
+        <div class="w-16 h-3 bg-neutral-500"></div>
+        <div class="w-16 h-3 bg-neutral-500"></div>
+    </div>
+    <div class="flex flex-col mt-12">
+        <p class="text-3xl">Hi, I'm</p>
+        <p class="text-6xl mt-1 font-extrabold">Cooper</p>
+    </div>
+</div>
