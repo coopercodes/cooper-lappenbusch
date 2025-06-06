8b1f6af2f176cb2332839bed42923c06791700d8
coopercodes3 <thecoopercodes@gmail.com>
2025-06-06 14:01:38 -0700

    initial ideas for port

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 </script>
 
 
-<div class="relative sticky left-1/2 transform -translate-x-1/2  flex items-center mt-[24px] py-2.5 w-[700px] bg-[#273F4F] rounded-[8px]">
+<div class="sticky top-[24px] z-50 left-1/2 transform -translate-x-1/2  flex items-center  py-2.5 w-[700px] bg-[#273F4F] rounded-[8px]">
     <div class="flex justify-between  mx-4 w-full">
         <div class="flex items-center">
             <p class="text-white font-bold text-lg">Cooper Lappenbusch</p>
@@ -15,7 +15,7 @@
             </div>
         </div>
 
-        <button class="bg-[#FE7743] hover:bg-black hover:text-[#FE7743] border border-transparent hover:border-[#FE7743]  transition-all duration-200 cursor-pointer px-4 text-white rounded-[8px] font-extrabold">
+        <button class="bg-[#FE7743] hover:bg-[#FE7743] hover:text-black border border-transparent hover:border-[#FE7743]  transition-all duration-200 cursor-pointer px-4 text-white rounded-[8px] font-extrabold">
             Reach Out
         </button>
     </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,13 @@
 
 
 
-<div class=" h-[800px]  pt-[100px] flex flex-col items-center ">
+<div class="  pt-[124px] flex flex-col items-center ">
 
     <div class="w-[700px] h-[400px] rounded-[10px] relative overflow-hidden shadow-2xl" 
             style="background-image: url('arcanaScreen.png'); background-size: cover; background-position: center;">
         
         <div class="absolute bottom-0 left-0 w-full h-32 bg-gradient-to-b to-black from-transparent to-[85%] rounded-bl-[10px]"></div>
         
-        <!-- Corner text only -->
         <div class="absolute bottom-5 left-8 text-white">
             <p class="text-xl font-bold mb-0.5 drop-shadow-lg">Arcana</p>
             <p class="text-sm opacity-90 drop-shadow-md">Time tracking in 3 clicks</p>
@@ -17,13 +16,113 @@
 
 
     <div class="flex space-x-4 mt-8">
-        <div class="w-16 h-3 bg-[#FE7743]"></div>
-        <div class="w-16 h-3 bg-neutral-500"></div>
-        <div class="w-16 h-3 bg-neutral-500"></div>
-        <div class="w-16 h-3 bg-neutral-500"></div>
+        <div class="w-16 h-3 bg-[#FE7743] cursor-pointer"></div>
+        <div class="w-16 h-3 bg-neutral-500  cursor-pointer"></div>
+        <div class="w-16 h-3 bg-neutral-500  cursor-pointer"></div>
+        <div class="w-16 h-3 bg-neutral-500  cursor-pointer"></div>
     </div>
-    <div class="flex flex-col mt-12">
-        <p class="text-3xl">Hi, I'm</p>
-        <p class="text-6xl mt-1 font-extrabold">Cooper</p>
+    <div class="flex  items-center  mt-16">
+
+    
+        <div class="flex flex-col">
+            <p class="text-3xl">Hi, I'm</p>
+            <p class="text-6xl font-extrabold">Cooper</p>
+        </div>
+        <div class="w-[1px] h-[110px] bg-black mx-16"></div>
+        <div class="flex flex-col">
+            <p class="text-3xl leading-10 mt-2">I'm a software engineer, educator, <br/>
+and product designer. </p>
+        </div>
+    </div>
+
+    <div class="my-32 h-[1px] w-[500px] bg-black flex items-center"></div>
+
+    <div class="w-[1200px] h-[500px] ">
+        <div class="flex h-full ">
+            <div class="flex flex-col justify-center w-[600px]  ">
+                <div class="flex items-center ">
+                    <div class="p-4 bg-neutral-800 w-full h-[400px] rounded-[12px] shadow-xl"></div>
+                </div>
+                <div class="flex items-center justify-center -mt-5 ">
+                    <div class="p-2 shadow-xl px-8 flex items-center justify-center bg-[#313131] w-max rounded-[12px]">
+                        <div class="flex space-x-8">
+                            <div class="h-5 w-5 rounded-full bg-[#FE7743] cursor-pointer">
+
+                            </div>
+                            <div class="h-5 w-5 rounded-full bg-[#A4A4A4] cursor-pointer">
+
+                            </div>
+                            <div class="h-5 w-5 rounded-full bg-[#A4A4A4] cursor-pointer">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex  w-full h-[72px] bg-[#273F4F] mt-4 items-center px-4 ">
+                    <p class="text-white font-extrabold leading-6.5 text-2xl">Tech <br/> Stack</p>
+                   
+                    <div class="flex items-center  w-full justify-around overflow-hidden">
+                         <div class="flex flex-col overflow-hidden items-center space-x-4 pl-2">
+                            <div class="w-[8px] h-[20px] bg-white"></div>
+                            <div class="w-[8px] h-[20px] bg-white"></div>
+                        </div>
+                        <p class="text-white leading-6.5 text-2xl">TypeScript</p>
+                        <p class="text-white leading-6.5 text-2xl">Rust</p>
+                        <p class="text-white leading-6.5 text-2xl">Svelte 5</p>
+                                                <p class="text-white leading-6.5 text-2xl">Tauri 2.0</p>
+
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="w-[1px] h-full bg-black mx-12"></div>
+            <div class="flex flex-col  w-[500px]">
+                <div class="flex items-center space-x-6">
+                    <p class="text-4xl font-bold">Arcana</p>
+                    <div class="w-full bg-black h-[1px]"></div>
+                    <button class="bg-[#FE7743] hover:bg-[#FE7743] hover:text-black border border-transparent hover:border-[#FE7743]  transition-all duration-200 cursor-pointer px-4 text-white rounded-[8px] font-bold text-nowrap">
+                        Read The Post
+                    </button>
+                </div>
+                <p class="text-xl mt-4 pr-0">
+                    Meet Arcana, my answer to the eternal question: "Where did 
+my day go?". Arcana is a timer that sits in the system tray of 
+your operating system, making it easy to click the Arcana icon,
+click your tag (ex: Studying),  and start tracking your time.
+
+                </p>
+                <div class="flex items-center my-4">
+                    <div class="w-full bg-black h-[1px] "></div>
+                    <div class="w-[50px]"></div>
+                    <div class="w-[1px] bg-black h-[32px] "></div>
+                </div>
+
+                <div class="flex">
+                    <div class="flex flex-col">
+                    <p class="text-xl font-bold pr-0">
+                    What I Learned
+                        </p>
+                        <p class="text-xl mt-2 pr-0">
+                            When building products, sometimes
+                            the most simple solutions are the
+                            most effective ones. <br/> <br/>
+                            By focusing on the most essential
+                            parts of building a product, such as
+                            time tracking and tags, you can 
+                            remove the other bloat that makes
+                            applications feel tiresome.
+                        </p>
+                    </div>
+                    <div class="bg-[#313131] flex items-end w-[300px] ml-6">
+                        <div class="bg-[#FE7743] w-[16px] h-[62px] m-4 ml-12"></div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+
+    <div class="my-32 h-[1px] w-[500px] bg-black flex items-center">
+        
     </div>
 </div>
